0d96e6a48ad36ac1bc2850755886cd6e28f5bf52
jonsia cejohn <jonsiacejohn@gmail.com>
2026-06-09 15:51:29 +0530

    Fix Render root route

--- server.js
+++ server.js
@@ -23,6 +23,14 @@ app.use("/api/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 5000;
 
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
